e02a56a0a7a6b7bc1fd36d24d3205411547abc85
Abdullajon <madraximovabdullajon806@gmail.com>
2026-04-16 00:28:44 +0500

    CoderAbuz

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
 import os
 
 # Telegram Bot Token
-BOT_TOKEN = "7944352953:AAGy3vEqM7gqwLJly0xs5upRX7CbGDaHJWA"
-
-# RapidAPI - Bu eng ishonchli yo'l. Sessionid kabi tez o'lib qolmaydi.
-# SIZNING OXIRGI SKRINSHOTDAGI AKTIV KALITINGIZ:
-RAPIDAPI_KEY = "62b161efd8mshb89646ba94f2865p11bc06jsnab8d7da726e"
-RAPIDAPI_HOST = "instagram120.p.rapidapi.com"
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7944352953:AAGy3vEqM7gqwLJly0xs5upRX7CbGDaHJWA")
 
 # Kataloglar
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
